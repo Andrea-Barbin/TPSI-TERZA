4dbc98ba088e0ff53d8a43911a4ddc6b9b9feaa8
Andrea-Barbin <121685379+Andrea-Barbin@users.noreply.github.com>
2023-02-03 16:36:58 +0100

    Add files via upload
    Compiti per le vacanze di Natale, esercizio Hello World

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+//ANDREA BARBIN 3°E ESERCIZIO PER LE VACANZE DI NATALE
+//Scrivere un programma in C che stampi a schermo la scritta "Hello World"
+
+//INIZIO
+#include<stdio.h>
+void main(){
+printf("Hello World");
+//FINE
+}
