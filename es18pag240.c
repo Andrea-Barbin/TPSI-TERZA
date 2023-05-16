e8be4993fc715ed874e3cbd1447cec6cac19e2b6
Andrea-Barbin <121685379+Andrea-Barbin@users.noreply.github.com>
2023-05-16 17:13:24 +0200

    Compiti per il 05-05-23
    Pag 240 es 18

---
+++ es18pag240.c
@@ -0,0 +1,14 @@
+//PAG 240 ES 18 
+
+void copiaContenuto (char *stringa1, char *stringa2) {
+    
+    while (*stringa1 != '\0') { 
+
+        if (*stringa1 != ' ') { 
+            *stringa2 = *stringa1;
+            stringa2++; 
+        }
+        stringa1++; 
+    }
+    *stringa2 = '\0'; 
+}
